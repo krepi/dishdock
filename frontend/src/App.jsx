07149a1135f8ae3e0bf12e2a7e12b8b0f3e9e5b5
przemyslawkrepski <krepski80@gmail.com>
2024-03-13 20:56:13 +0100

    some app frontend skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+import Home from "./Views/Home.jsx";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1> Welcome </h1>
+      <Home/>
 
     </>
   )
---
+++ frontend/src/Views/Home.jsx
@@ -0,0 +1,21 @@
+
+export default function Home ()
+{
+    return (
+        <>
+            <div>
+                <p>navbar</p>
+            </div>
+            <div>
+                <h1>Cześć z Home dishdocka</h1>
+            </div>
+            <main>
+                <p>main i body</p>
+            </main>
+            <footer>
+                <p>footer</p>
+            </footer>
+
+        </>
+    )
+}
