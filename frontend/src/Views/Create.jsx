5ae5fbae635851b8ce257d14f8fb56dd2b95c7d1
przemyslawkrepski <krepski80@gmail.com>
2024-03-18 11:21:28 +0100

    some work with Create

--- frontend/src/Views/Create.jsx
+++ frontend/src/Views/Create.jsx
@@ -1,5 +1,21 @@
 const Create = () => (
     <>
-    <h1>Create</h1>
+        <h1>Create</h1>
+        <form action="recipe/save" method="POST">
+            <label htmlFor="name">name</label>
+            <input type="text" id='name' name='name'/>
+            <label htmlFor="">ingredients</label>
+            <input type="text"/>
+            <label htmlFor="">steps</label>
+            <input type="text"/>
+            <label htmlFor="">kalories</label>
+            <input type="text"/>
+            <label htmlFor="">servings</label>
+            <input type="text"/>
+            <label htmlFor="">tips</label>
+            <input type="text"/>
+            <button type="submit">Submit</button>
+        </form>
+
     </>
 )
