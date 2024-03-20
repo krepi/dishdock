0ca592a3c3780fa2bfb3e1680241a4cb25bb6465
przemyslawkrepski <krepski80@gmail.com>
2024-03-20 19:21:59 +0100

    Footer created

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,8 +1,12 @@
 
 const Footer = () => (
-    <>
-    <h2>Footer </h2>
-    </>
+    <footer className="bg-gray-900 flex  text-gray-500 p-2 ">
+        {/* Treść stopki */}
+        <div className="max-w-7xl mx-auto flex justify-between items-center">
+            <p>&copy; DishDock {new Date().getFullYear()}</p>
+            {/* Możesz dodać więcej treści, jak linki czy informacje o prawach autorskich */}
+        </div>
+    </footer>
 );
 
 export default Footer;
--- frontend/src/Views/Home.jsx
+++ frontend/src/Views/Home.jsx
@@ -1,14 +1,11 @@
-import Header from "../Components/Header.jsx";
-import Main from "../Components/Main.jsx";
 import Footer from "../Components/Footer.jsx";
-import Create from "./Recipes/Create.jsx";
-import Edit from "./Recipes/Edit.jsx";
 import WelcomeContent from "./WelcomeContent/WelcomeContent.jsx";
 
 export default function Home() {
     return (
         <>
             <WelcomeContent/>
+            <Footer/>
         </>
     )
 }
