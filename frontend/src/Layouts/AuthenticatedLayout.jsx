4f0ea0e474779e9e9372c04745bff5f7e6795298
przemyslawkrepski <krepski80@gmail.com>
2024-03-27 20:03:33 +0100

    AuthenticatedLayout  with children have been added

--- frontend/src/Layouts/AuthenticatedLayout.jsx
+++ frontend/src/Layouts/AuthenticatedLayout.jsx
@@ -1,9 +1,14 @@
-const AuthenticatedLayout = ()=>{
+import Header from "../Components/Header.jsx";
+import Footer from "../Components/Footer.jsx";
+
+const AuthenticatedLayout = ({children}) => {
 
 
     return (
         <>
-            
+            <Header/>
+            <main className='flex-1'>{children}</main>
+            <Footer/>
         </>
     )
 }
--- frontend/src/Views/Home.jsx
+++ frontend/src/Views/Home.jsx
@@ -1,13 +1,15 @@
 import Footer from "../Components/Footer.jsx";
 import WelcomeContent from "./WelcomeContent/WelcomeContent.jsx";
 import Header from "../Components/Header.jsx";
+import AuthenticatedLayout from "../Layouts/AuthenticatedLayout.jsx";
 
 export default function Home() {
     return (
         <>
-            <Header/>
-            <WelcomeContent/>
-            <Footer/>
+            <AuthenticatedLayout>
+                <WelcomeContent/>
+            </AuthenticatedLayout>
+
         </>
     )
 }
