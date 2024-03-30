f95d7cc862d4ec3fc9fa79c022dd838577c3faa5
przemyslawkrepski <krepski80@gmail.com>
2024-03-30 11:43:59 +0100

    first recipe fetched

--- backend/hello-world.js
+++ backend/hello-world.js
@@ -1,12 +1,37 @@
-const http = require('node:http');
+import dotenv from 'dotenv';
+dotenv.config();
+import axios from 'axios';
+
+
+import http from "node:http";
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+const getRecipe = async () => {
+    const apiKey = process.env.API_KEY_SPOON; // Użyj klucza API z pliku .env
+    const url = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=1`; // URL do pobrania losowego przepisu
+
+    try {
+        const response = await axios.get(url); // Wykonaj zapytanie do API przy użyciu axios
+        return response.data; // axios automatycznie przetwarza odpowiedź jako JSON
+    } catch (error) {
+        console.error('Error fetching recipe:', error);
+        return null; // W przypadku błędu zwróć null
+    }
+};
+
+const server = http.createServer(async (req, res) => {
+    if (req.url === '/recipe') { // Dodaj ścieżkę /recipe
+        const recipeData = await getRecipe(); // Pobierz dane przepisu
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(recipeData)); // Wyślij dane przepisu jako JSON
+    } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello, World!\n');
+    }
 });
 
 server.listen(port, hostname, () => {
